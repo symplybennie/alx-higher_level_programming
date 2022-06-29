20c524f27d6337574adaaf3adaafc961eeb7247f
symply$bennie <46108655+symplybennie@users.noreply.github.com>
2022-06-29 15:57:10 +0100

    Update 3-print_alphabt.py
    fix

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for c in range(ord('a'), ord('z') + 1):
-    if c != ord('e') and c != ord('q'):
-       print("{:c}".format(c), end="")
+
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
